f1df0f438aa1d5bf9fec6b9ce2a76781fa69d880
SubhashreeDisha <152278134+SubhashreeDisha@users.noreply.github.com>
2023-11-28 12:26:45 +0000

    file added

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+/*1-variables is a container to store some data.
+2-There are three types of variables in our javascript
+     1-let
+     2-var                              
+     3-const
+     var vs let
+     1-   var
+     ------------------------------------
+      a-we used before 2015 to assign some variable.
+      b-used in functional scope.
+
+    2-    let
+      ------------------------------------
+      a-after Es6 we declare variable by use of let.
+      b-used in blockscope.
+      */
