c99d598f1cbb2462cdd054303b6602de1893c0e1
SubhashreeDisha <152278134+SubhashreeDisha@users.noreply.github.com>
2023-12-15 06:30:59 +0000

    l[';/

---
+++ 01_basic/07_Dom.js
@@ -0,0 +1,29 @@
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+
+//     <title>Dom learning</title>
+//     <style>
+//         .bg-black{
+//             background-color: #212121;
+//             color: #fff;
+//         }
+//     </style>
+// </head>
+// <body class="bg-black">
+//     <div>
+//         <h1 id="title" class="heading">learning at Web_Bocket<span style="display: none;">later on added text</span></h1>
+//         <h2>Hi hello world</h2>
+//         <h2>My name is Saneay</h2>
+//         <p>Lorem ipsum dolor sit amet</p>
+//         <input type="password" name = "" id = "">Enter your password
+
+//         <ul>
+//             <li class="list-item">one<li>
+//             <li class="list-item">Two</li>
+//             <li class="list-item">Three</li>
+//         </ul>
+//     </div>
+// </body>
+// </html>
